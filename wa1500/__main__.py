8994c4b28628df27be0a80cc188bc969f028a2e3
Shreyas Potnis <shreyaspotnis@gmail.com>
2016-01-06 16:29:30 -0500

    Basic serial communication, should do something

---
+++ wa1500/__main__.py
@@ -0,0 +1,27 @@
+import time
+import serial
+
+# init RS232 port COM1, 2400 baud, 8 data bits, no
+# parity, 1 stop bit
+
+wa1500 = serial.Serial(port='COM1', baudrate=2400,
+                       parity=serial.PARITY_NONE,
+                       stopbits=serial.STOPBITS_TWO,
+                       bytesize=serial.EIGHTBITS)
+
+
+print 'Is port open?', wa1500.isOpen()
+
+input = '@Q'
+wa1500.write(input + '\r\n')
+out = ''
+# let's wait one second before reading output
+# (let's give device time to answer)
+time.sleep(1)
+while wa1500.inWaiting() > 0:
+    out += wa1500.read(1)
+
+if out != '':
+    print ">>" + out
+
+wa1500.close()

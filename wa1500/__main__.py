757cf49cd466738013b86136093ed77153238228
Leonardo Uribe <leo@ubuntu.(none)>
2016-02-12 20:56:15 +0000

    fix indentation

--- wa1500/__main__.py
+++ wa1500/__main__.py
@@ -72,26 +72,25 @@ class WA1500_dummy:
 done = False
 wavemeter_define = False
 while not done:
-  try:
-     wavemeter = WA1500('/dev/ttyUSB0')
-     wavemeter_define = True
-     while True:
-        topic = 'wa1500'  # This will be useful when there are multiple streams
-                          # to watch
-        freq, err_msg = wavemeter.read_frequency()
-        dt = str(datetime.datetime.now())
-        send_string = "%s %s %f %s" % (topic, dt, freq, err_msg)
-        print(send_string)
-        pub_socket.send(send_string)
-        time.sleep(0.1)
- except KeyboardInterrupt:
-    print wavemeter.close()
-    pub_socket.close()
-    done = True
- except serial.serialutil.SerialException as e:
-     print "Serial Exception: ", e
-     if wavemeter_define:
-         wavemeter.close()
-         wavemeter_define = False
-         
-     time.sleep(1.0)
+    try:
+        wavemeter = WA1500('/dev/ttyUSB0')
+        wavemeter_define = True
+        while True:
+            topic = 'wa1500'  # This will be useful when there are multiple streams
+                              # to watch
+            freq, err_msg = wavemeter.read_frequency()
+            dt = str(datetime.datetime.now())
+            send_string = "%s %s %f %s" % (topic, dt, freq, err_msg)
+            print(send_string)
+            pub_socket.send(send_string)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print wavemeter.close()
+        pub_socket.close()
+        done = True
+    except serial.serialutil.SerialException as e:
+        print "Serial Exception: ", e
+        if wavemeter_define:
+            wavemeter.close()
+            wavemeter_define = False
+        time.sleep(1.0)

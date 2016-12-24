d8b116b83931754d854cc3925c61b4c56fa4bd8c
Shreyas Potnis <shreyaspotnis@gmail.com>
2016-12-24 18:29:49 -0500

    zmq pub moved to a new object

--- wa1500/__main__.py
+++ wa1500/__main__.py
@@ -1,4 +1,4 @@
-"""Reads and publishes the wa1500 wavemeter an a zeromq socket."""
+"""Reads and publishes the wa1500 wavemeter reading an a zeromq socket."""
 
 import time
 import serial
@@ -24,19 +24,35 @@ parser.add_argument("-p", "--publishport", type=int,
 parser.add_argument("-t", "--topic", type=str,
                     help='topic to use when broadcasting. e.g wa1500-lab1',
                     default='wa1500')
+parser.add_argument("--dummy", action='store_true',
+                    help='Do not read wavemeter, publish random'
+                         ' numbers instead.')
 
 args = parser.parse_args()
 
 
-publish_port = args.publishport
-topic = args.topic
-serial_device_path = args.serialport
+class zmq_pub_dict:
+    """Publishes a python dictionary on a port with a given topic."""
 
-print('Broadcasting on port {0} with topic {1}'.format(publish_port, topic))
+    def __init__(self, port, topic):
+        zmq_context = zmq.Context()
+        self.topic = topic
+        self.pub_socket = zmq_context.socket(zmq.PUB)
+        self.pub_socket.bind("tcp://*:%s" % port)
+        print('Broadcasting on port {0} with topic {1}'.format(port,
+                                                               topic))
 
-zmq_context = zmq.Context()
-pub_socket = zmq_context.socket(zmq.PUB)
-pub_socket.bind("tcp://*:%s" % publish_port)
+    def send(self, data_dict):
+        timestamp = time.time()
+        send_string = "%s %f %s" % (self.topic, timestamp, repr(data_dict))
+        print(send_string)
+        self.pub_socket.send(send_string)
+
+    def close(self):
+        self.pub_socket.close()
+
+
+publisher = zmq_pub_dict(args.publishport, args.topic)
 
 
 class WA1500:
@@ -101,22 +117,21 @@ done = False
 wavemeter_defined = False
 while not done:
     try:
-        wavemeter = WA1500(serial_device_path)
+        if args.dummy:
+            wavemeter = WA1500_dummy(args.serialport)
+        else:
+            wavemeter = WA1500(args.serialport)
         wavemeter_defined = True
         while True:
             freq, err_msg = wavemeter.read_frequency()
-            timestamp = time.time()
-            data_dict = {'timestamp': timestamp,
-                         'freq': freq,
+            data_dict = {'freq': freq,
                          'err_msg': err_msg}
-            send_string = "%s %f %s" % (topic, timestamp, repr(data_dict))
-            print(send_string)
-            pub_socket.send(send_string)
+            publisher.send(data_dict)
+
             time.sleep(0.5)
     except KeyboardInterrupt as e:
         print "KeyboardInterrupt: exiting"
         print wavemeter.close()
-        pub_socket.close()
         done = True
     except serial.serialutil.SerialException as e:
         print "SerialException: ", e
@@ -124,3 +139,5 @@ while not done:
             wavemeter.close()
             wavemeter_defined = False
         time.sleep(1.0)
+
+publisher.close()
